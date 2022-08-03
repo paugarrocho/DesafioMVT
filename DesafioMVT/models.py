94681ba9011da5624b8c611aa250300621738128
Paula Garrocho <55091571+paugarrocho@users.noreply.github.com>
2022-08-02 21:46:01 -0300

    Delete DesafioMVT directory

--- DesafioMVT/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Familiar(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    relacion = models.CharField(max_length=30)
-    fechaNac = models.DateField()
